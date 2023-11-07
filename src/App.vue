2677c57f9479d9fedaa138fd6a2328c13bc81083
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2023-11-07 09:30:09 +0100

    props

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>{{title}}</h1>
+<p>Welcome ... </p>
+<div v-if="showModal">
+
+  <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+</div>
+
+<button @click.shift="toggleModal" >open modal</button>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal},
+  data(){
+    return{
+      title:'NetNinja Tutorial',
+      header: 'Sign up for the Giveaway!',
+      text: 'Grab your ninja swag for half price',
+      showModal: false
+
+    }
+  },
+
+  methods:{
+    toggleModal(){
+      this.showModal = !this.showModal
+    }
   }
+
+
 }
 </script>
 
@@ -22,5 +46,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+
 }
 </style>
